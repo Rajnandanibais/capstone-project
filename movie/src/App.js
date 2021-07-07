e858d3660f513f7d539f6d852c75c58b6be03081
Rajnandanibais <rajnandanisinghbais@gmail.com>
2021-07-07 14:36:38 +0530

    first commit

---
+++ movie/src/App.js
@@ -0,0 +1,19 @@
+import Movie from "./component/Movie";
+import React from "react";
+
+const featuredApi="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+ const imgApi="https://image.tmdb.org/t/p/w1280";
+ const searchApi="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+          
+    </div>
+  );
+}
+
+export default App;
